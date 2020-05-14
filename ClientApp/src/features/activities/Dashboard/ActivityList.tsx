23a15f772b2a0fd46cf59b6dc24c50d29a3a6564
Shiv singh kushwah <shiv.kushwaha@compunnel.com>
2020-05-14 23:30:49 +0530

    End of Section 8

--- ClientApp/src/features/activities/Dashboard/ActivityList.tsx
+++ ClientApp/src/features/activities/Dashboard/ActivityList.tsx
@@ -1,42 +1,48 @@
-import React, { useContext } from "react";
-import { Item, Button, Label, Segment } from "semantic-ui-react";
-import { observer } from "mobx-react-lite";
+import React, { useContext } from 'react';
+import { Item, Button, Label, Segment } from 'semantic-ui-react';
+import { observer } from 'mobx-react-lite';
 import ActivityStore from '../../../app/stores/activityStore';
+import { Link } from 'react-router-dom';
 
 const ActivityList: React.FC = () => {
-
   const activityStore = useContext(ActivityStore);
-  const {activitiesByDate,selectActivity,deleteActivity,target,submitting} = activityStore;
-  return (    
+  const {
+    activitiesByDate,
+    deleteActivity,
+    submitting,
+    target
+  } = activityStore;
+  return (
     <Segment clearing>
       <Item.Group divided>
-        {activitiesByDate.map((activity) => (
+        {activitiesByDate.map(activity => (
           <Item key={activity.id}>
             <Item.Content>
-              <Item.Header as="a">{activity.title}</Item.Header>
+              <Item.Header as='a'>{activity.title}</Item.Header>
               <Item.Meta>{activity.date}</Item.Meta>
               <Item.Description>
                 <div>{activity.description}</div>
                 <div>
-                  {activity.city} , {activity.venue}
+                  {activity.city}, {activity.venue}
                 </div>
               </Item.Description>
               <Item.Extra>
                 <Button
-                  onClick={() => selectActivity(activity.id)}
-                  floated="right"
-                  content="view"
-                  color="blue"
-                ></Button>
+                  as={Link}
+                  to={`/activities/${activity.id}`}
+                  floated='right'
+                  content='View'
+                  color='blue'
+                />
                 <Button
                   name={activity.id}
                   loading={target === activity.id && submitting}
-                  onClick={(event) => deleteActivity(event,activity.id)}
-                  floated="right"
-                  content="Delete"
-                  color="red"
-                ></Button>
-                <Label basic content={activity.category}></Label>
+                  onClick={e => deleteActivity(e, activity.id)}
+                  floated='right'
+                  content='Delete'
+                  color='red'
+                />
+                <Label basic content={activity.category} />
               </Item.Extra>
             </Item.Content>
           </Item>
