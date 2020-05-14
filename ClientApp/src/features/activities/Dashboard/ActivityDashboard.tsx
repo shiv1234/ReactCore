5c9c0d6ba0f4147dc7cb3c347b6b6e8410a20be8
Shiv singh kushwah <shiv.kushwaha@compunnel.com>
2020-05-15 00:27:22 +0530

    End of Section 9

--- ClientApp/src/features/activities/Dashboard/ActivityDashboard.tsx
+++ ClientApp/src/features/activities/Dashboard/ActivityDashboard.tsx
@@ -1,6 +1,6 @@
 import React, { useContext, useEffect } from 'react';
 import { Grid } from 'semantic-ui-react';
-import ActivityList from './ActivityList'
+import ActivityList from './ActivityList';
 import { observer } from 'mobx-react-lite';
 import ActivityStore from '../../../app/stores/activityStore';
 import LoadingComponent from '../../../app/layout/LoadingComponent';
