9144000648a6e2c9e1e4122be5ea6048a2f02eef
Shiv singh kushwah <shiv.kushwaha@compunnel.com>
2020-05-23 00:44:03 +0530

    End  Of Section 11

--- ClientApp/src/features/activities/Details/ActivityDetails.tsx
+++ ClientApp/src/features/activities/Details/ActivityDetails.tsx
@@ -1,13 +1,13 @@
-import React, { useContext, useEffect } from "react";
-import { Grid } from "semantic-ui-react";
-import ActivityStore from "../../../app/stores/activityStore";
-import { observer } from "mobx-react-lite";
-import { RouteComponentProps } from "react-router";
-import LoadingComponent from "../../../app/layout/LoadingComponent";
-import ActivityDetailedHeader from "./ActivityDetailedHeader";
-import ActivityDetailedInfo from "./ActivityDetailedInfo";
-import ActivityDetailedChat from "./ActivityDetailedChat";
-import ActivityDetailedSidebar from "./ActivityDetailedSidebar";
+import React, { useContext, useEffect } from 'react';
+import { Grid } from 'semantic-ui-react';
+import ActivityStore from '../../../app/stores/activityStore';
+import { observer } from 'mobx-react-lite';
+import { RouteComponentProps } from 'react-router';
+import LoadingComponent from '../../../app/layout/LoadingComponent';
+import ActivityDetailedHeader from './ActivityDetailedHeader';
+import ActivityDetailedInfo from './ActivityDetailedInfo';
+import ActivityDetailedChat from './ActivityDetailedChat';
+import ActivityDetailedSidebar from './ActivityDetailedSidebar';
 
 interface DetailParams {
   id: string;
@@ -15,16 +15,18 @@ interface DetailParams {
 
 const ActivityDetails: React.FC<RouteComponentProps<DetailParams>> = ({
   match,
-  history,
+  history
 }) => {
   const activityStore = useContext(ActivityStore);
   const { activity, loadActivity, loadingInitial } = activityStore;
 
   useEffect(() => {
     loadActivity(match.params.id);
-  }, [loadActivity, match.params.id]);
-  if (loadingInitial || !activity)
-    return <LoadingComponent content="Loading activity..." />;
+  }, [loadActivity, match.params.id, history]);
+
+  if (loadingInitial) return <LoadingComponent content='Loading activity...' />;
+
+  if (!activity) return <h2>Activity not found</h2>;
 
   return (
     <Grid>
