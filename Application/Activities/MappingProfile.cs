ad938b216beec46fed51b9dfe1fbc1136ccc3c8d
Shiv singh kushwah <shiv.kushwaha@compunnel.com>
2020-06-08 17:26:02 +0530

    End of Section 16

--- Application/Activities/MappingProfile.cs
+++ Application/Activities/MappingProfile.cs
@@ -1,3 +1,4 @@
+using System.Linq;
 using AutoMapper;
 using Domain;
 
@@ -10,7 +11,8 @@ namespace Application.Activities
             CreateMap<Activity, ActivityDto>();
             CreateMap<UserActivity, AttendeeDto>()
             .ForMember(x => x.UserName, o => o.MapFrom(s => s.AppUser.UserName))
-            .ForMember(x=>x.DisplayName , o => o.MapFrom(s => s.AppUser.DisplayName));
+            .ForMember(x=>x.DisplayName , o => o.MapFrom(s => s.AppUser.DisplayName))
+            .ForMember(x=>x.Image , o => o.MapFrom(s => s.AppUser.Photos.FirstOrDefault(p => p.IsMain).Url));
         }
     }
 }
